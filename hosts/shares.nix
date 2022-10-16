016585974de75a1e366af7ef02659df38929238e
oluceps <i@oluceps.uk>
2022-10-16 14:15:10 +0800

    ref: hosts
    Former-commit-id: d641c1d91ec9d4df2b4959d3b128047275f37657

---
+++ hosts/shares.nix
@@ -0,0 +1,33 @@
+{inputs, system, pkgs }:
+[
+  ../misc.nix
+  ../users.nix
+  ../packages.nix
+  ../sysvars.nix
+  ../services.nix
+  {
+    environment.systemPackages = with inputs;
+      [
+        alejandra.defaultPackage.${system}
+        agenix.defaultPackage.${system}
+        helix.packages.${system}.default
+        surrealdb.packages.${system}.default
+
+      ] ++ (with pkgs;[
+        (fenix.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ])
+        rust-analyzer-nightly
+      ]);
+  }
+] ++ (with inputs;[
+
+  agenix.nixosModule
+  grub2-themes.nixosModule
+  home-manager.nixosModules.home-manager
+
+]) ++ (import ../modules)
