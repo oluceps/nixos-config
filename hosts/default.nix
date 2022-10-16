27c18d92f5365fa432a5a0a01c9f0a18a1caf204
oluceps <i@oluceps.uk>
2022-10-16 14:15:10 +0800

    ref: hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,60 +1,49 @@
-{ inputs, system, ... }:
-
+{ inputs, pkgss }:
 let
   nixosSystem = inputs.nixpkgs.lib.nixosSystem;
-
-  pkgs = import inputs.nixpkgs-unstable {
-    inherit system;
-    config.allowUnfree = true;
-    overlays = [
-      (final: prev: {
-        nur-pkgs = inputs.nur-pkgs.packages."${prev.system}";
-      })
-      inputs.nur.overlay
-    ] ++ (import ../overlay.nix inputs);
-
-  };
-
-  sharedModules = [
-    ../misc.nix
-    ../users.nix
-    ../boot.nix
-    ../packages.nix
-    ../sysvars.nix
-    ../services.nix
-    {
-      environment.systemPackages = with inputs; [
-        alejandra.defaultPackage.${system}
-        agenix.defaultPackage.${system}
-        helix.packages.${system}.default
-      ];
-    }
-  ] ++ (with inputs;[
-
-    agenix.nixosModule
-    grub2-themes.nixosModule
-    home-manager.nixosModules.home-manager
-
-  ]) ++ (import ../modules);
-
-
 in
 {
   hastur = nixosSystem (
-    let user = "riro"; in
+
+    let
+      system = "x86_64-linux";
+      pkgs = pkgss.${system};
+      user = "riro";
+    in
     {
       inherit system pkgs;
       specialArgs = { inherit inputs system user; };
-      modules = (import ./hastur) ++ sharedModules;
+      modules = (import ./hastur) ++ (import ./shares.nix { inherit system pkgs inputs; });
     }
   );
 
   kaambl = nixosSystem (
-    let user = "elena"; in
+
+    let
+      system = "x86_64-linux";
+      pkgs = pkgss.${system};
+      user = "elena";
+    in
     {
       inherit system pkgs;
       specialArgs = { inherit inputs system user; };
-      modules = (import ./kaambl) ++ sharedModules;
+      modules = (import ./kaambl) ++ (import ./shares.nix { inherit system pkgs inputs; });
     }
+  );
+
+  livecd = nixosSystem (
+    let
+      user = "isho";
+      system = "x86_64-linux";
+      pkgs = pkgss.${system};
+    in
+    {
+      inherit system pkgs;
+      specialArgs = { inherit inputs system user; };
+      modules = (import ./livecd) ++ (import ./shares.nix { inherit system pkgs inputs; }) ++
+        [ (inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix") ]
+      ;
+    }
+    # OMG 7.3G ISO 
   );
 }
