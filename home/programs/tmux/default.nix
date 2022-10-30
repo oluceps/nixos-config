37272d713f5c01327e6e1c148668b539f19255da
oluceps <i@oluceps.uk>
2022-10-30 14:42:31 +0800

    add: tmux config hm
    Former-commit-id: 877ef999d81cbbeaee78c784bbbd1c7317032162

---
+++ home/programs/tmux/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    escapeTime = 10;
+    shell = "${pkgs.fish}/bin/fish";
+    keyMode = "vi";
+    terminal = "screen-256color";
+    extraConfig = ''
+      set -g status-position top
+      set -g set-clipboard on
+      set -g mouse on
+      set -g status-right ""
+      set -g renumber-windows on
+      set -ga terminal-overrides ",alacritty:Tc"
+      new-session -s main
+      set -sg escape-time 0
+    '';
+
+  };
+}
