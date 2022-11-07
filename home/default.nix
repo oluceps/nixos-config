561e7bb083ab815cb81e6347fe0b1815555b0d89
oluceps <i@oluceps.uk>
2022-11-07 18:12:17 +0800

    add: hyprland

--- home/default.nix
+++ home/default.nix
@@ -11,8 +11,8 @@ in
     users.${user} = {
       imports = [
         homeProfile
-#        inputs.hyprland.homeManagerModules.default
-#        ../modules/hyprland
+        inputs.hyprland.homeManagerModules.default
+        ../modules/hyprland
         #        
       ];
     };
