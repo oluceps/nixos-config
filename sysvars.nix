092aa983dd522a4ecbf78badfe54e7ab73ebb6a9
oluceps <i@oluceps.uk>
2022-11-25 19:20:20 +0800

    rm: MOZ_ENABLE_WAYLAND

--- sysvars.nix
+++ sysvars.nix
@@ -8,9 +8,6 @@
     NEOVIDE_WM_CLASS = "1";
     EDITOR = "hx";
     NIXOS_OZONE_WL = "1";
-
-    MOZ_ENABLE_WAYLAND = "1";
-
     NODE_PATH = "~/.npm-packages/lib/node_modules";
     PATH = [
       "\${XDG_BIN_HOME}"
