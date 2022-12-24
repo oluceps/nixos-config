9b233185f6c5cfccfdfa7b366df7062773788323
oluceps <i@oluceps.uk>
2022-12-24 17:59:09 +0800

    mv: persist config

---
+++ hosts/hastur/persist.nix
@@ -0,0 +1,42 @@
+{ user, ... }: {
+
+  environment.persistence."/persist" = {
+    directories = [
+      "/etc/nixos"
+      "/etc/ssh"
+      "/var/log"
+      "/var/lib"
+    ];
+    users.${user} = {
+
+      files = [
+        ".npmrc"
+      ];
+      directories = [
+        "Documents"
+        "Downloads"
+        "Pictures"
+        "Projects"
+        "Videos"
+        "Music"
+        "Security"
+        "softwares"
+        "Blog"
+        "Games"
+        ".npm-packages"
+        "tools"
+        "Vault"
+        "calibreLib"
+        ".cache"
+        ".local"
+        ".mozilla"
+        ".config"
+        ".FeelUOwn"
+        { directory = ".gnupg"; mode = "0700"; }
+        { directory = ".ssh"; mode = "0700"; }
+        { directory = ".yubico"; mode = "0700"; }
+      ];
+    };
+  };
+}
+  
