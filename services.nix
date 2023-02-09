aeed81f544b0b28b0b4537abfc58582fa5264f81
oluceps <i@oluceps.uk>
2023-02-09 15:11:43 +0800

    btrfs: scrub location
    Former-commit-id: cc73ceb836c55c0acbc15fe3aca0474157b7c078

--- services.nix
+++ services.nix
@@ -136,7 +136,7 @@
     btrfs.autoScrub = {
       enable = true;
       interval = "monthly";
-      fileSystems = [ "/home" "/nix" ];
+      fileSystems = [ "/persist" "nix" ];
     };
     pcscd.enable = true;
 
