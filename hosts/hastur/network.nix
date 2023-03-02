d27de3440f40d2d2246f5156b7d9307a62947a48
oluceps <i@oluceps.uk>
2023-03-03 02:07:04 +0800

    add: wg config
    add: wg key
    
    fix: wg key path
    
    Former-commit-id: 8d807b1451e8dd6868b72986d5ab68b99d156e96

--- hosts/hastur/network.nix
+++ hosts/hastur/network.nix
@@ -15,30 +15,30 @@
     # proxy.default = "http://127.0.0.1:7890";
     networkmanager.enable = false;
 
-    # wireguard.interfaces = {
-    #   wg0 = {
-    #     ips = [ "172.16.0.2/32" "fd01:5ca1:ab1e:88ba:9158:7ec7:1f13:7535/128" ];
-    #     listenPort = 51820;
-    #     privateKeyFile = "/home/${user}/.config/wg/key";
-    #     peers = [
-    #       {
-    #         #             Public key of the server (not a file path).
-    #         publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+    wireguard.interfaces = {
+      wg0 = {
+        ips = [ "172.16.0.2/32" "fd01:5ca1:ab1e:88ba:9158:7ec7:1f13:7535/128" ];
+        listenPort = 51820;
+        privateKeyFile = config.age.secrets.wg.path;
+        peers = [
+          {
+            #             Public key of the server (not a file path).
+            publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
 
-    #         #          Forward all the traffic via VPN.
-    #         allowedIPs = [ "0.0.0.0/1" "::/1" ];
-    #         #           Or forward only particular subnets
-    #         #           allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+            #          Forward all the traffic via VPN.
+            allowedIPs = [ "172.16.0.0/24" ];
+            #           Or forward only particular subnets
+            #           allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
-    #         #            Set this to the server IP and port.
-    #         endpoint = "162.159.192.8:2408"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuardLoop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+            #            Set this to the server IP and port.
+            endpoint = "162.159.192.8:2408"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuardLoop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
-    #         #             Send keepalives every 25 seconds. Important to keep NAT tables alive.
-    #         persistentKeepalive = 25;
-    #       }
-    #     ];
-    #   };
-    # };
+            #             Send keepalives every 25 seconds. Important to keep NAT tables alive.
+            persistentKeepalive = 25;
+          }
+        ];
+      };
+    };
   };
   systemd.network = {
     enable = true;
