89b0cc66b5a85cc9a2a8824e91d4ee2c52484b8f
oluceps <i@oluceps.uk>
2023-03-14 14:17:36 +0800

    fix: flake overlays
    Former-commit-id: 5de300ade87eb054cdd5fbe7a5bbbc4a53482c11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,50 @@
 {
   description = "flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-22.url = "github:NixOS/nixpkgs?rev=c91d0713ac476dfb367bbe12a7a048f6162f039c";
+    nixpkgs-pin-kernel.url = "github:NixOS/nixpkgs/master";
+    nil.url = "github:oxalica/nil";
+    nix-direnv.url = "github:nix-community/nix-direnv";
+    nix-colors.url = "github:misterio77/nix-colors";
+    clansty.url = "github:clansty/flake";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur.url = "github:nix-community/NUR";
+    nur-pkgs = {
+      url = "github:oluceps/nur-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+    surrealdb.url = "github:surrealdb/surrealdb";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    clash-meta.url = "github:MetaCubeX/Clash.Meta/Alpha";
+    alejandra.url = "github:kamadorueda/alejandra";
+    polymc.url = "github:PolyMC/PolyMC";
+    prismlauncher = {
+      url = "github:PrismLauncher/PrismLauncher";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pywmpkg.url = "github:jbuchermn/pywm";
+    home-manager.url = "github:nix-community/home-manager";
+    helix.url = "github:helix-editor/helix";
+    hyprland.url = "github:vaxerski/Hyprland";
+    grub2-themes.url = "github:vinceliuice/grub2-themes";
+    mach-nix.url = "mach-nix/3.5.0";
+    colmena.url = "github:zhaofengli/colmena";
+    berberman.url = "github:berberman/flakes";
+  };
+
   outputs = { self, ... }@inputs:
     let
       genSystems = inputs.nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
@@ -47,52 +92,8 @@
           dirContents = builtins.readDir ./pkgs;
           names = builtins.attrNames dirContents;
         in
-        prev.lib.genAttrs names (name: final.callPackage ("./pkgs/${name}") { });
+        prev.lib.genAttrs names (name: final.callPackage (./pkgs + "/${name}") { });
 
     };
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-22.url = "github:NixOS/nixpkgs?rev=c91d0713ac476dfb367bbe12a7a048f6162f039c";
-    nixpkgs-pin-kernel.url = "github:NixOS/nixpkgs/master";
-    nil.url = "github:oxalica/nil";
-    nix-direnv.url = "github:nix-community/nix-direnv";
-    nix-colors.url = "github:misterio77/nix-colors";
-    clansty.url = "github:clansty/flake";
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nur.url = "github:nix-community/NUR";
-    nur-pkgs = {
-      url = "github:oluceps/nur-pkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprpicker.url = "github:hyprwm/hyprpicker";
-    surrealdb.url = "github:surrealdb/surrealdb";
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence.url = "github:nix-community/impermanence";
-    clash-meta.url = "github:MetaCubeX/Clash.Meta/Alpha";
-    alejandra.url = "github:kamadorueda/alejandra";
-    polymc.url = "github:PolyMC/PolyMC";
-    prismlauncher = {
-      url = "github:PrismLauncher/PrismLauncher";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pywmpkg.url = "github:jbuchermn/pywm";
-    home-manager.url = "github:nix-community/home-manager";
-    helix.url = "github:helix-editor/helix";
-    hyprland.url = "github:vaxerski/Hyprland";
-    grub2-themes.url = "github:vinceliuice/grub2-themes";
-    mach-nix.url = "mach-nix/3.5.0";
-    colmena.url = "github:zhaofengli/colmena";
-    berberman.url = "github:berberman/flakes";
-  };
 }
