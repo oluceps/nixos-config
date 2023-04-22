6f4eff11b0b661165d9edf2b4cb1d0c0f8bdd621
oluceps <i@oluceps.uk>
2023-04-22 14:15:22 +0800

    clean
    Former-commit-id: 3f3ed5adf84fca2ce476675cd87bca2d3cb0977a

--- misc.nix
+++ misc.nix
@@ -153,11 +153,11 @@
     };
 
   environment = {
-    shellInit = ''
-      export GPG_TTY="$(tty)"
-      gpg-connect-agent /bye
-      export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-    '';
+    # shellInit = ''
+    #   export GPG_TTY="$(tty)"
+    #   gpg-connect-agent /bye
+    #   export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+    # '';
     loginShellInit = "";
   };
   # Set your time zone.
