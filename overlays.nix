15ec1a593503f2d6e948d3939aa8e7db98138c5b
oluceps <i@oluceps.uk>
2023-04-30 18:32:47 +0800

    sec: add functions

--- overlays.nix
+++ overlays.nix
@@ -31,7 +31,7 @@
         includeGrammarIf = grammar:
           prev.lib.any
             (name: grammar.name == name)
-            [ "toml" "rust" "nix" "protobuf" "yaml" "json" "markdown" "html" "css" "zig" "c" "cpp" "go" "python" "bash" "kotlin" ];
+            [ "toml" "rust" "nix" "protobuf" "yaml" "json" "markdown" "html" "css" "zig" "c" "cpp" "go" "python" "bash" "kotlin" "fish" ];
       };
 
 
