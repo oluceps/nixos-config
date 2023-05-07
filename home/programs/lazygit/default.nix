f5e562f221824120028abdb670aa88fda11ecf58
oluceps <i@oluceps.uk>
2023-05-07 08:23:24 +0800

    add: lazygit
    Former-commit-id: 58b2845e91818094fa2797d1df5cb58250443f05

---
+++ home/programs/lazygit/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.lazygit ];
+  xdg.configFile."lazygit/config.yml".text = ''
+    os:
+    edit: 'hx {{filename}}'
+    editAtLine: 'hx {{filename}}:{{line}}'
+    editAtLineAndWait: 'hx {{filename}}:{{line}}'
+    editInTerminal: true
+  '';
+}
