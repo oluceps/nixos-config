4671b5e70648146a31680fb5ffbbb61dabbc92b9
oluceps <i@oluceps.uk>
2023-05-11 20:45:57 +0800

    reformat

--- flake.nix
+++ flake.nix
@@ -54,7 +54,19 @@
       genOverlays = map (let m = i: inputs.${i}.overlays; in (i: (m i).default or (m i).${i})); # ugly
 
       # contentAddressedByDefault = true;
-      _pkgs = genSystems (system: import inputs.nixpkgs { inherit system; config = { allowUnfree = true; allowBroken = false; segger-jlink.acceptLicense = true; allowUnsupportedSystem = true; permittedInsecurePackages = [ "python-2.7.18.6" "electron-21.4.0" ]; }; overlays = (import ./overlays.nix { inherit inputs system; }) ++ genOverlays [ "self" "clansty" "fenix" "berberman" "nvfetcher" ] ++ [ inputs.nur.overlay ]; }); #（>﹏<）
+      _pkgs = genSystems (system: import inputs.nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          allowBroken = false;
+          segger-jlink.acceptLicense = true;
+          allowUnsupportedSystem = true;
+          permittedInsecurePackages = [ "python-2.7.18.6" "electron-21.4.0" ];
+        };
+        overlays = (import ./overlays.nix { inherit inputs system; })
+          ++ genOverlays [ "self" "clansty" "fenix" "berberman" "nvfetcher" ]
+          ++ [ inputs.nur.overlay ]; #（>﹏<）
+      });
 
       genericImport = p: import p { inherit inputs _pkgs; };
     in
@@ -63,11 +75,19 @@
 
       devShells.x86_64-linux = genericImport ./shells.nix;
 
-      apps = genSystems (system: inputs.agenix-rekey.defineApps self _pkgs.${system} { inherit (self.nixosConfigurations) hastur kaambl; });
+      apps =
+        genSystems (system: inputs.agenix-rekey.defineApps self _pkgs.${system}
+          { inherit (self.nixosConfigurations) hastur kaambl; });
 
-      overlays.default = final: prev: prev.lib.genAttrs (with builtins;(attrNames (readDir ./pkgs))) (name: final.callPackage (./pkgs + "/${name}") { });
+      overlays.default =
+        final: prev: prev.lib.genAttrs (with builtins;(attrNames (readDir ./pkgs)))
+          (name: final.callPackage (./pkgs + "/${name}") { });
 
-      checks = genSystems (system: with _pkgs.${system}; { pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run { src = lib.cleanSource ./.; hooks = { nixpkgs-fmt.enable = true; }; }; });
+      checks = genSystems (system: with _pkgs.${system};
+        {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run
+            { src = lib.cleanSource ./.; hooks = { nixpkgs-fmt.enable = true; }; };
+        });
 
     };
 
