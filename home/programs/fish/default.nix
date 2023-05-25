f629ece2f9aef30e1d49c5d526863174a4011edb
oluceps <i@oluceps.uk>
2023-05-26 00:33:59 +0800

    chore
    Former-commit-id: e1887efdf4bdf29a17451cf81a58970d0edca10f

--- home/programs/fish/default.nix
+++ home/programs/fish/default.nix
@@ -83,6 +83,7 @@
         description = "edit agenix-rekey secret";
       };
       dec = "rage -d -i /run/agenix/age $argv[1]";
+      enc = "rage -i /etc/nixos/sec/age-yubikey-identity-7d5d5540.txt.pub -R /run/agenix/pub -e $argv[1] -o $argv[1].age";
       extract = ''
         set --local ext (echo $argv[1] | awk -F. '{print $NF}')
         switch $ext
--- overlays.nix
+++ overlays.nix
@@ -53,6 +53,7 @@
               "diff"
               "comment"
               "vue"
+              "nu"
             ];
       };
 
