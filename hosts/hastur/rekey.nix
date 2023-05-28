3d49372bcbc2f7ff030eaf7415c91b18fb5eb8b9
oluceps <i@oluceps.uk>
2023-05-28 23:52:50 +0800

    chore

--- hosts/hastur/rekey.nix
+++ hosts/hastur/rekey.nix
@@ -2,5 +2,9 @@
 {
   age.identityPaths = [ "/persist/keys/ssh_host_ed25519_key" ];
   rekey.hostPubkey = data.keys.hasturHostPubKey;
+  services.openssh.hostKeys = [{
+    path = "/persist/keys/ssh_host_ed25519_key";
+    type = "ed25519";
+  }];
 }
 
