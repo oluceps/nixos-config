bd439cec6b59d7b56ce65c720f5edaf8b28e303f
oluceps <i@oluceps.uk>
2023-06-06 01:29:06 +0800

    fix: winrun

--- pkgs/windows-run/default.nix
+++ pkgs/windows-run/default.nix
@@ -7,9 +7,13 @@ pkgs.writeShellApplication {
       -machine q35 -accel kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
       -smp sockets=1,cores=6 -m 8G \
       -vga qxl \
-      -display gtk \
+      -device virtio-serial-pci \
+      -spice port=5900,disable-ticketing=on \
+      -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+      -chardev spicevmc,id=spicechannel0,name=vdagent \
       -nic user,model=virtio-net-pci \
       -drive if=virtio,file=/var/lib/libvirt/images/win10.qcow2,aio=io_uring \
       "$@"
   '';
 }
+# -display gtk \
