481997724422488bbd4b558d187cb5666558bf26
oluceps <i@oluceps.uk>
2023-06-28 03:26:59 +0800

    term: -> wezterm

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -2,6 +2,5 @@
 
   home.packages = [ pkgs.wezterm ];
   xdg.configFile."wezterm/wezterm.lua".text = builtins.readFile ./wezterm.lua;
-
   xdg.configFile."wezterm/catppuccin.lua".text = builtins.readFile ./catppuccin.lua;
 }
