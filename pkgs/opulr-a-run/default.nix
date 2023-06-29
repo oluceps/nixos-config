4f5774e3025c51733d59744fe1438d780df3ab18
oluceps <i@oluceps.uk>
2023-06-30 05:03:01 +0800

    fix: OPEuler aarch64 qemu boot script

--- pkgs/opulr-a-run/default.nix
+++ pkgs/opulr-a-run/default.nix
@@ -1,12 +1,12 @@
-{ pkgs, inputs }:
+{ qemu, writeShellApplication, fd_iuBrGE }:
 
-pkgs.writeShellApplication {
+writeShellApplication {
   name = "opulr-a-run";
   text = ''
-    ${pkgs.qemu}/bin/qemu-system-aarch64 \
+    ${qemu}/bin/qemu-system-aarch64 \
     -cpu cortex-a57 \
     -machine virt -nographic -m 8192 -smp 22 \
-    -bios ${(import inputs.nixpkgs-22 {system = "x86_64-linux";}).pkgsCross.aarch64-multiplatform.OVMF.fd}/FV/QEMU_EFI.fd \
+    -bios ${fd_iuBrGE}/FV/QEMU_EFI.fd \
     -device virtio-net-device,netdev=usernet \
     -netdev user,id=usernet,hostfwd=tcp::12055-:22 \
     -device qemu-xhci -usb -device usb-kbd -device usb-tablet \
