c1fc672bc5e92aabf0b88f2e2992504c49ef1daf
oluceps <i@oluceps.uk>
2023-08-15 02:51:09 +0800

    try: fix gnome
    Former-commit-id: e2276a30cc30f230db048bf5505ed66b89939af1

--- hosts/kaambl/spec.nix
+++ hosts/kaambl/spec.nix
@@ -28,5 +28,7 @@
       desktopManager.gnome.enable = true;
     };
   };
+  environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 
 }
