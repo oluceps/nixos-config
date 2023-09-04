ebd64313522291af89272da0ed1fc924bbe30d11
oluceps <i@oluceps.uk>
2023-09-05 01:05:31 +0800

    toolbox
    Former-commit-id: 06a71c923116657eb941312872bd1be25164efb0

--- copy.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-# copy agenix-rekey secrets to remote
-
-nix copy --substitute-on-destination --to ssh://rhs \
-  $(nix eval --raw .#nixosConfigurations.kaambl.config.age.rekey.derivation)
