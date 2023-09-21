1a3d9f3a0a6fd515a4aa01b6bd8dfd5cb554def7
oluceps <i@oluceps.uk>
2023-09-22 03:14:47 +0800

    chore

--- services.nix
+++ services.nix
@@ -210,7 +210,7 @@
               concurrent = 2;
               upstreams = [
                 { addr = "tls://8.8.8.8:853"; idle_timeout = 86400; enable_pipeline = true; }
-                { addr = "tls://1.0.0.1:853"; idle_timeout = 86400; enable_pipeline = true; }
+                { addr = "tls://8.8.4.4:853"; idle_timeout = 86400; enable_pipeline = true; }
               ];
             };
             tag = "remote_forward";
