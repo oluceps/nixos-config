f25c4da3fd8c75774e1c358eca98ab38577596c2
oluceps <i@oluceps.uk>
2023-10-09 15:12:42 +0800

    + psgre remote access ctl

--- hosts/hastur/spec.nix
+++ hosts/hastur/spec.nix
@@ -64,14 +64,18 @@
       enableTCPIP = true;
       port = 5432;
       authentication = pkgs.lib.mkOverride 10 ''
-        #...
+        #type database  DBuser  auth-method
+        local all       all     trust
+
         #type database DBuser origin-address auth-method
         # ipv4
         host  all      all     127.0.0.1/32   trust
-        host  all      all     10.0.1.1/24    trust
+        host  all      all     10.0.1.1/24   trust
+        host  all      all     10.0.0.1/24   trust
         # ipv6
         host all       all     ::1/128        trust
       '';
+
     };
 
     pipewire = {
