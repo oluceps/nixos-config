dae54b1d5377885c133010fd0008784a851f2670
oluceps <i@oluceps.uk>
2023-10-17 01:13:18 +0800

    * hostname style
    Former-commit-id: 26a4d0b8be9b8c82d38707a9376b51d788b0a4b9

--- hosts/yidhra/spec.nix
+++ hosts/yidhra/spec.nix
@@ -15,16 +15,8 @@
   };
 
   programs = {
-
     git.enable = true;
     fish.enable = true;
-
-    starship = {
-      enable = true;
-      settings = (import ../../home/programs/starship { }).programs.starship.settings // {
-        format = "$username$directory$git_branch$git_commit$git_status$nix_shell$cmd_duration$line_break$python$character";
-      };
-    };
   };
   zramSwap = {
     enable = true;
