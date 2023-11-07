b5343929043c257b084bfa7c3f842afd6091e626
oluceps <i@oluceps.uk>
2023-11-07 16:25:00 +0800

    ~
    Former-commit-id: 4affff842eedfe25e87a8b833fbe86e8730d14bb

--- hosts/default.nix
+++ hosts/default.nix
@@ -12,133 +12,136 @@ let share = { inherit genOverlays sharedModules base lib; }; in [
   (import
     ./azasos
     share)
+  (import
+    ./nodens
+    share)
 
   ./livecd
 
-  ({ ... }: {
-    flake = { pkgs, ... }: {
-      colmena = {
-        meta = {
-          nixpkgs = import inputs.nixpkgs {
-            system = "x86_64-linux";
-            config = {
-              allowUnfree = true;
-              allowBroken = false;
-            };
-            overlays = (import ../overlays.nix { inherit inputs; })
-              ++
-              (genOverlays [
-                "self"
-                # "clansty"
-                "fenix"
-                "berberman"
-                "nvfetcher"
-                "EHfive"
-                "nuenv"
-                "agenix-rekey"
-                "nixyDomains"
-              ]);
-          };
-        };
+  # ({ ... }: {
+  #   flake = { pkgs, ... }: {
+  #     colmena = {
+  #       meta = {
+  #         nixpkgs = import inputs.nixpkgs {
+  #           system = "x86_64-linux";
+  #           config = {
+  #             allowUnfree = true;
+  #             allowBroken = false;
+  #           };
+  #           overlays = (import ../overlays.nix { inherit inputs; })
+  #             ++
+  #             (genOverlays [
+  #               "self"
+  #               # "clansty"
+  #               "fenix"
+  #               "berberman"
+  #               "nvfetcher"
+  #               "EHfive"
+  #               "nuenv"
+  #               "agenix-rekey"
+  #               "nixyDomains"
+  #             ]);
+  #         };
+  #       };
 
-        # yidhra = { pkgs, nodes, ... }:
-        #   let user = "elen"; in {
-        #     deployment = {
-        #       targetHost = "lsa";
-        #     };
+  #       # yidhra = { pkgs, nodes, ... }:
+  #       #   let user = "elen"; in {
+  #       #     deployment = {
+  #       #       targetHost = "lsa";
+  #       #     };
 
-        #     imports =
-        #       (map (n: ./yidhra + ("/" + n)) [
-        #         "hardware.nix"
-        #         "network.nix"
-        #         "spec.nix"
-        #       ]) ++
+  #       #     imports =
+  #       #       (map (n: ./yidhra + ("/" + n)) [
+  #       #         "hardware.nix"
+  #       #         "network.nix"
+  #       #         "spec.nix"
+  #       #       ]) ++
 
-        #       (
-        #         let a = { inherit data lib user; }; in [
-        #           (import ./yidhra/rekey.nix data)
-        #           (import ../age.nix a)
-        #           (import ../users.nix ({ inherit pkgs; } // a))
-        #           (import ../packages.nix (
-        #             ({
-        #               inherit pkgs;
-        #               inherit (base) self lib;
-        #               config = nodes.yidhra.config;
-        #             })
-        #           ))
-        #           # (import ../misc.nix (
-        #           #   ({
-        #           #     inherit pkgs inputs data user;
-        #           #     inherit (base) self lib;
-        #           #     config = nodes.yidhra.config;
-        #           #   })
-        #           # ))
-        #         ]
-        #       )
-        #       ++ sharedModules;
-        #   };
+  #       #       (
+  #       #         let a = { inherit data lib user; }; in [
+  #       #           (import ./yidhra/rekey.nix data)
+  #       #           (import ../age.nix a)
+  #       #           (import ../users.nix ({ inherit pkgs; } // a))
+  #       #           (import ../packages.nix (
+  #       #             ({
+  #       #               inherit pkgs;
+  #       #               inherit (base) self lib;
+  #       #               config = nodes.yidhra.config;
+  #       #             })
+  #       #           ))
+  #       #           # (import ../misc.nix (
+  #       #           #   ({
+  #       #           #     inherit pkgs inputs data user;
+  #       #           #     inherit (base) self lib;
+  #       #           #     config = nodes.yidhra.config;
+  #       #           #   })
+  #       #           # ))
+  #       #         ]
+  #       #       )
+  #       #       ++ sharedModules;
+  #       #   };
 
-        nodens = { pkgs, nodes, ... }:
-          let user = "elen"; in {
-            deployment = {
-              targetHost = "dgs";
-            };
+  #       nodens = { pkgs, nodes, ... }:
+  #         let user = "elen"; in {
+  #           deployment = {
+  #             targetHost = "dgs";
+  #           };
 
-            imports =
-              (map (n: ./nodens + ("/" + n)) [
-                "hardware.nix"
-                "network.nix"
-                "spec.nix"
-              ]) ++
+  #           imports =
+  #             (map (n: ./nodens + ("/" + n)) [
+  #               "hardware.nix"
+  #               "network.nix"
+  #               "spec.nix"
+  #             ]) ++
 
-              (
-                let a = { inherit data lib user; }; in [
-                  (import ./nodens/rekey.nix data)
-                  (import ../age.nix a)
-                  (import ../users.nix ({ inherit pkgs; } // a))
-                  (import ../packages.nix (
-                    ({
-                      inherit pkgs;
-                      inherit (base) self lib;
-                      config = nodes.nodens.config;
-                    })
-                  ))
-                ]
-              )
-              ++ sharedModules;
-          };
+  #             (
+  #               let a = { inherit data lib user; }; in [
+  #                 (import ./nodens/rekey.nix data)
+  #                 (import ../age.nix a)
+  #                 (import ../users.nix ({ inherit pkgs; } // a))
+  #                 (import ../packages.nix (
+  #                   ({
+  #                     inherit pkgs;
+  #                     inherit (base) self lib;
+  #                     config = nodes.nodens.config;
+  #                   })
+  #                 ))
+  #               ]
+  #             )
+  #             ++ sharedModules;
+  #         };
 
-        azasos = { pkgs, nodes, ... }:
-          let user = "elen"; in {
-            deployment = {
-              targetHost = "tcs";
-            };
+  #       azasos = { pkgs, nodes, ... }:
+  #         let user = "elen"; in {
+  #           deployment = {
+  #             targetHost = "tcs";
+  #           };
 
-            imports =
-              (map (n: ./azasos + ("/" + n)) [
-                "hardware.nix"
-                "network.nix"
-                "spec.nix"
-              ]) ++
+  #           imports =
+  #             (map (n: ./azasos + ("/" + n)) [
+  #               "hardware.nix"
+  #               "network.nix"
+  #               "spec.nix"
+  #             ]) ++
 
-              (
-                let a = { inherit data lib user; }; in [
-                  (import ./azasos/rekey.nix data)
-                  (import ../age.nix a)
-                  (import ../users.nix ({ inherit pkgs; } // a))
-                  (import ../packages.nix (
-                    ({
-                      inherit pkgs;
-                      inherit (base) self lib;
-                      config = nodes.azasos.config;
-                    })
-                  ))
-                ]
-              )
-              ++ sharedModules;
-          };
-      };
-    };
+  #             (
+  #               let a = { inherit data lib user; }; in [
+  #                 (import ./azasos/rekey.nix data)
+  #                 (import ../age.nix a)
+  #                 (import ../users.nix ({ inherit pkgs; } // a))
+  #                 (import ../packages.nix (
+  #                   ({
+  #                     inherit pkgs;
+  #                     inherit (base) self lib;
+  #                     config = nodes.azasos.config;
+  #                   })
+  #                 ))
+  #               ]
+  #             )
+  #             ++ sharedModules;
+  #         };
+  #     };
+  #   };
 
-  })
+  # })
 ]
