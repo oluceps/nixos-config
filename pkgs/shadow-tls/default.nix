beb6ca824b0aebad1e017a4a7cc8ad6515dddcc2
oluceps <i@oluceps.uk>
2023-11-16 15:11:13 +0800

    +

--- pkgs/shadow-tls/default.nix
+++ pkgs/shadow-tls/default.nix
@@ -1,36 +1,24 @@
-{ lib
-, fetchFromGitHub
+{ fetchFromGitHub
 , pkgs
-  #, fenix
+, rustPlatform
 }:
 
-let
-  #  fenix = import
-  #    (fetchTarball {
-  #      url = "https://github.com/nix-community/fenix/archive/main.tar.gz";
-  #      sha256 = "sha256:1l0j43iks97lk70s3zb43vfbbjf1j1x2nrpbfx559xfv0ivn45br";
-  #    })
-  #    { };
-  rustPlatform = pkgs.makeRustPlatform { inherit (pkgs.fenix.minimal) cargo rustc; };
-in
+# let
+# rustPlatform = pkgs.makeRustPlatform { inherit (pkgs.fenix.minimal) cargo rustc; };
+# in
 rustPlatform.buildRustPackage rec{
   pname = "shadow-tls";
-  version = "0.2.3";
+  version = "0.2.23-unstable";
 
   src = fetchFromGitHub {
-    rev = "v${version}";
+    rev = "3f4d3124b665b62d5bac44025a2781c729612a2a";
     owner = "ihciah";
     repo = pname;
-    sha256 = "sha256-jNXXICQf7ukX+lU4pjOf5OL99ymKVxkq52IcZPWQY8E=";
+    hash = "sha256-XMoNCSSj76aGJzGatOudwWO21qimlgeRMGNUmzxzM6I=";
   };
-
-  cargoSha256 = "sha256-dV5MKsmpSxQxKUe6WKf6IfNQo/YGD9JYhWu7Rv58nN4=";
-
-  meta = with lib; {
-    homepage = "https://github.com/ihciah/shadow-tls";
-    description = ''
-      A proxy to expose real tls handshake to the firewall.
-    '';
-    #    maintainers = with maintainers; [ oluceps ];
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+    };
   };
 }
