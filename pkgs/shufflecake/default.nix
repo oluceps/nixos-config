e8e53a52d532e619982d93cbef81ecd25803a099
oluceps <i@oluceps.uk>
2023-12-20 23:14:46 +0800

    ~

--- pkgs/shufflecake/default.nix
+++ pkgs/shufflecake/default.nix
@@ -1,28 +1,30 @@
-{ kernel, stdenv, fetchFromGitea, libgcrypt, lvm2 }:
+{ lib, kernel, stdenv, fetchFromGitea, libgcrypt, lvm2 }:
 let
-  genShufflecake = name: installPhase: stdenv.mkDerivation (finalAttrs: {
-    inherit name;
-    version = "0.4.3";
-    src = fetchFromGitea {
-      domain = "codeberg.org";
-      owner = "shufflecake";
-      repo = finalAttrs.name;
-      rev = "v${finalAttrs.version}";
-      hash = "sha256-aBLa0puoOrrsJAFSFANtQr3oH/HXAZ809rQeBC4nHxs=";
-    };
+  genShufflecake = name: installPhase: {
+    ${name} = stdenv.mkDerivation (finalAttrs: {
+      inherit name;
+      version = "0.4.3";
+      src = fetchFromGitea {
+        domain = "codeberg.org";
+        owner = "shufflecake";
+        repo = finalAttrs.name;
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-aBLa0puoOrrsJAFSFANtQr3oH/HXAZ809rQeBC4nHxs=";
+      };
 
-    nativeBuildInputs = kernel.moduleBuildDependencies;
-    buildInputs = [ libgcrypt lvm2 ];
-    makeFlags = kernel.makeFlags ++ [
-      "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-      "INSTALL_MOD_PATH=$(out)"
-    ];
-    inherit installPhase;
-  });
+      nativeBuildInputs = kernel.moduleBuildDependencies;
+      buildInputs = [ libgcrypt lvm2 ];
+      makeFlags = kernel.makeFlags ++ [
+        "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+        "INSTALL_MOD_PATH=$(out)"
+      ];
+      inherit installPhase;
+    });
+  };
+
 in
-{
-  shufflecake-c =
-    genShufflecake "shufflecake-c" "install -D dm-sflc.ko $out/lib/modules/${kernel.modDirVersion}/misc/dm-sflc.ko";
-  shufflecake-userland =
-    genShufflecake "shufflecake" "install -D shufflecake $out/bin/shufflecake";
+builtins.mapAttrs (name: value: genShufflecake name value) {
+  shufflecake-c = "install -D dm-sflc.ko $out/lib/modules/${kernel.modDirVersion}/misc/dm-sflc.ko";
+  shufflecake = "install -D shufflecake $out/bin/shufflecake";
 }
+
