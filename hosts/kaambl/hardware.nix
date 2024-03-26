49c36bd32ad82a64110a20a0dca545b3f3147850
oluceps <i@oluceps.uk>
2024-03-27 05:35:36 +0800

    -

--- hosts/kaambl/hardware.nix
+++ hosts/kaambl/hardware.nix
@@ -164,7 +164,7 @@
       #   system = "x86_64-linux";
       # })
       # pkgs.linuxPackages_latest;
-      inputs.nyx.packages.${pkgs.system}.linuxPackages_cachyos-zen3;
+      inputs.nyx.packages.${pkgs.system}.linuxPackages_cachyos;
 
     # kernelPatches =
     #   let patchPath = ../../.attachs/cachyos-kernel;
