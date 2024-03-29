1bed517aae06b301a1c61ee6cecc212a1c719316
oluceps <i@oluceps.uk>
2024-03-30 00:57:50 +0800

    +
    Former-commit-id: 8ccffadf955eb7a6fe9f6d4b7321f3e434de4229 [formerly d1857e2ff3feddb21f1054c2ff8fb9ecc92e55f8]
    Former-commit-id: d16c1c7217f70e97affd6902ef9af1e9fde4e0e5

--- hosts/resq/additions.nix
+++ hosts/resq/additions.nix
@@ -20,9 +20,9 @@
       # sudo mkdir /run/systemd/system/nix-daemon.service.d/
       # cat << EOF >/run/systemd/system/nix-daemon.service.d/override.conf  
       # [Service]
-      # Environment="http_proxy=socks5h://localhost:7891"
-      # Environment="https_proxy=socks5h://localhost:7891"
-      # Environment="all_proxy=socks5h://localhost:7891"
+      # Environment="http_proxy=socks5h://localhost:1900"
+      # Environment="https_proxy=socks5h://localhost:1900"
+      # Environment="all_proxy=socks5h://localhost:1900"
       # EOF
       # sudo systemctl daemon-reload
       # sudo systemctl restart nix-daemon
@@ -125,6 +125,8 @@
     tor
     iperf3
     i2p
+    git
+    tmux
     ethtool
     dnsutils
     tcpdump
