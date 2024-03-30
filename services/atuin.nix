e0a71b8f10884f362c004d7a7c683171554f8921
oluceps <i@oluceps.uk>
2024-03-30 20:09:37 +0800

    -
    Former-commit-id: 9d97e023dbb9b589b12f077823843611dbe4acb8 [formerly dbbb04e58c11d371134f22c629dfc93fb981cdd5]
    Former-commit-id: a5821cc0e5e2278794e8b76aadda83a8bfb445c4

--- services/atuin.nix
+++ services/atuin.nix
@@ -3,7 +3,7 @@
   host = "0.0.0.0";
   port = 8888;
   openFirewall = true;
-  openRegistration = true;
+  openRegistration = false;
   maxHistoryLength = 65536;
   database.uri = "postgresql://atuin@127.0.0.1:5432/atuin";
 }
