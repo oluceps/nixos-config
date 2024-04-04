b13034d977f5689ab1c970f02cbe899b4be0594f
oluceps <i@oluceps.uk>
2024-04-04 18:48:36 +0800

    ~
    Former-commit-id: 3eb822ef8555432bd6064e08b9762489369480c9 [formerly 0f924af456a485c7d864cb4182701b2576edb5d9]
    Former-commit-id: 887a7ab565f5c99bf979b2ee9e59159fde841969

--- hosts/hastur/spec.nix
+++ hosts/hastur/spec.nix
@@ -262,27 +262,5 @@
       region = "ap-east-1";
       rootCredentialsFile = config.age.secrets.minio.path;
     };
-
-    xmrig = {
-      enable = false;
-      settings = {
-        autosave = true;
-        opencl = false;
-        cuda = false;
-        cpu = {
-          enable = true;
-          max-threads-hint = 90;
-        };
-        pools = [
-          {
-            url = "pool.supportxmr.com:443";
-            user = data.xmrAddr;
-            keepalive = true;
-            tls = true;
-            pass = "rha";
-          }
-        ];
-      };
-    };
   };
 }
