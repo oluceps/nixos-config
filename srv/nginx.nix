3d3583979701df3844144dd0c1e5e5713a70a58f
oluceps <i@oluceps.uk>
2024-04-06 01:49:58 +0800

    ~
    Former-commit-id: 75cb835f269b25e6205da0cdded2b5570f96ccb5 [formerly 50ca4512e9884d412a6be28144d8c75b7c07e088]
    Former-commit-id: be618bec46fd0b2c16ab33d5c37195aa16664b64

--- srv/nginx.nix
+++ srv/nginx.nix
@@ -10,13 +10,6 @@
       locations = {
         "/" = {
           proxyPass = "http://localhost:8083";
-          #   # required when the target is also TLS server with multiple hosts
-
-          #   "proxy_ssl_server_name on;"
-          #   +
-          #     # required when the server wants to use HTTP Authentication
-
-          #     "proxy_pass_header Authorization;";
         };
       };
     };
