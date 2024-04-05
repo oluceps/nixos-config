4f5be6b7f1bffc197f654bb912dab5681a291f39
oluceps <i@oluceps.uk>
2024-04-05 18:21:05 +0800

    ~
    Former-commit-id: fbcebdf9679182b19bd67e3f3110d8195e1919b3 [formerly 4cfc685554ae8bfde5e0a3756c961847a1b756ae]
    Former-commit-id: 9a2325b6ce3abb1fe650b9b656b013a52100cb6c

--- hosts/azasos/network.nix
+++ hosts/azasos/network.nix
@@ -117,7 +117,7 @@
           {
             wireguardPeerConfig = {
               PublicKey = "+fuA9nUmFVKy2Ijfh5xfcnO9tpA/SkIL4ttiWKsxyXI=";
-              AllowedIPs = [ "10.0.0.0/24" ];
+              AllowedIPs = [ "10.0.0.0/16" ];
               Endpoint = "127.0.0.1:41820";
               PersistentKeepalive = 15;
             };
