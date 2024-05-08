203765fa846dc7179cd67c6bb038500e91e0c995
oluceps <i@oluceps.uk>
2024-05-08 13:59:02 +0000

    +
    Former-commit-id: b02ff1af5435dc2902732f9583d84ac36c6ac7c6 [formerly 2392a55392b435c73272aaa857fc18ac466c01e7]
    Former-commit-id: 2b47e3154be4551aafae356e1d082db36abae805

---
+++ home/programs/mpv/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  mpv = {
+    enable = true;
+    scripts = [ pkgs.mpvScripts.uosc ];
+    config = {
+      vo = "gpu-next";
+      hwdec = "auto-safe";
+      gpu-api = "vulkan";
+      gpu-context = "waylandvk";
+    };
+    bindings = {
+      "r" = "cycle_values video-rotate 90 180 270 0";
+    };
+  };
+}
