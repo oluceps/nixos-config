f35660b890e1cad2163c2156a4743c4f9dfdd9be
oluceps <i@oluceps.uk>
2024-07-05 11:14:59 +0000

    -

--- hosts/kaambl/hardware.nix
+++ hosts/kaambl/hardware.nix
@@ -27,7 +27,7 @@
     disk = {
       nvme = {
         type = "disk";
-        device = "/dev/disk/by-id/nvme-HP_SSD_FX900_Plus_M.2_2TB_HBSE53120600733";
+        device = "/dev/disk/by-id/nvme-HP_SSD_FX900_Plus_2TB_HBSE53360805325";
         content = {
           type = "gpt";
           partitions = {
