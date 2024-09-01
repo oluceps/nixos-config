23580201e2eecaef743fdf75aad3d3a2b264a2bd
oluceps <i@oluceps.uk>
2024-09-02 02:46:49 +0800

    +

--- hosts/nodens/caddy.nix
+++ hosts/nodens/caddy.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -18,6 +19,56 @@
           servers = {
             srv0 = {
               routes = [
+                {
+                  handle = [
+                    {
+                      handler = "subroute";
+                      routes = [
+                        {
+                          handle = [
+                            {
+                              handler = "reverse_proxy";
+                              upstreams = [ { dial = "10.0.1.2:6167"; } ];
+                            }
+                          ];
+                          match = [ { path = [ "/_matrix/*" ]; } ];
+                        }
+                        {
+                          handle = [
+                            {
+                              handler = "headers";
+                              response.set = {
+                                X-Frame-Options = [ "SAMEORIGIN" ];
+                                X-Content-Type-Options = [ "nosniff" ];
+                                X-XSS-Protection = [ "1; mode=block" ];
+                                Content-Security-Policy = [ "frame-ancestors 'self'" ];
+                              };
+
+                            }
+                            (
+                              let
+                                conf = {
+                                  default_server_config = {
+                                    "m.homeserver" = {
+                                      base_url = "https://matrix.nyaw.xyz";
+                                      server_name = "nyaw.xyz";
+                                    };
+                                  };
+                                  show_labs_settings = true;
+                                };
+                              in
+                              {
+                                handler = "file_server";
+                                root = "${pkgs.element-web.override { inherit conf; }}";
+                              }
+                            )
+                          ];
+                        }
+                      ];
+                    }
+                  ];
+                  match = [ { host = [ "matrix.nyaw.xyz" ]; } ];
+                }
                 {
                   handle = [
                     {
