0596e7fd06e35f84576e236f879c3828a60e963d
oluceps <i@oluceps.uk>
2024-09-08 00:06:30 +0800

    +

--- flake.nix
+++ flake.nix
@@ -254,7 +254,7 @@
     nixyDomains.flake = false;
     nuenv.url = "github:DeterminateSystems/nuenv";
     agenix-rekey = {
-      url = "github:oddlama/agenix-rekey?rev=55e05291c2d7";
+      url = "github:oddlama/agenix-rekey";
       # url = "/home/elen/Src/agenix-rekey";
       inputs.nixpkgs.follows = "nixpkgs";
     };
