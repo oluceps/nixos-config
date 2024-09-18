09571745426fac9fd7634734b3c0751e9183750a
oluceps <i@oluceps.uk>
2024-09-18 10:12:12 +0800

    +
    +

--- hosts/kaambl/spec.nix
+++ hosts/kaambl/spec.nix
@@ -175,15 +175,15 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      jack.enable = true;
-      extraConfig.pipewire."92-low-latency" = {
-        context.properties = {
-          default.clock.rate = 48000;
-          default.clock.quantum = 256;
-          default.clock.min-quantum = 256;
-          default.clock.max-quantum = 256;
-        };
-      };
+      # jack.enable = true;
+      # extraConfig.pipewire."92-low-latency" = {
+      #   context.properties = {
+      #     default.clock.rate = 48000;
+      #     default.clock.quantum = 256;
+      #     default.clock.min-quantum = 256;
+      #     default.clock.max-quantum = 256;
+      #   };
+      # };
     };
   };
 
--- hosts/sysvars.nix
+++ hosts/sysvars.nix
@@ -14,7 +14,6 @@
     AWS_SHARED_CREDENTIALS_FILE = config.age.secrets.aws-s3-cred.path;
     GOPATH = "\${HOME}/.cache/go";
     QT_IM_MODULES = "fcitx;wayland";
-    QT_QPA_PLATFORMTHEME = "gtk3";
     # NIX_CFLAGS_COMPILE = "--verbose";
     # NIX_CFLAGS_LINK = "--verbose";
     # NIX_LDFLAGS = "--verbose";
