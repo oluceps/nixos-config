0a7cb6386acf8ccfb2b4406775c663abe4ce14e2
oluceps <secinia@outlook.com>
2024-09-23 15:04:50 +0800

    +

--- pkgs/pico-rng.nix
+++ pkgs/pico-rng.nix
@@ -3,7 +3,6 @@
   stdenv,
   fetchFromGitHub,
   cmake,
-  pico-sdk,
 }:
 stdenv.mkDerivation (finalAttrs: {
   name = "pico-rng";
@@ -15,13 +14,19 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   sourceRoot = "${finalAttrs.src.name}/driver";
-  nativeBuildInputs = kernel.moduleBuildDependencies ++ [ cmake ];
-  makeFlags = kernel.makeFlags ++ [
-    "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-  ];
-  # cmakeFlags = [
-  #   "-DPICO_SDK_PATH=${pico-sdk}/lib/pico-sdk"
+  # makeFlags = kernel.makeFlags ++ [
+    # "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    # "INSTALL_MOD_PATH=$(out)"
   # ];
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p $out/src
+    cp pico_rng.c $out/src
+    cp Kbuild $out/src
+    make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build M=$out/src modules
+    runHook postBuild
+  '';
+  nativeBuildInputs = kernel.moduleBuildDependencies;
   installPhase = ''
     install -D pico_rng.ko $out/lib/modules/${kernel.modDirVersion}/driver/pico_rng.ko
   '';
