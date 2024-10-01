2e7128167568d8f2f497f8cdf0a4899aa7324d3e
oluceps <secinia@outlook.com>
2024-10-02 01:06:21 +0800

    -

--- repack/samba.nix
+++ repack/samba.nix
@@ -1,5 +1,14 @@
 { reIf, ... }:
 reIf {
+  avahi = {
+    publish.enable = true;
+    publish.userServices = true;
+    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    nssmdns4 = true;
+    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+    enable = true;
+    openFirewall = true;
+  };
   services.samba = {
     enable = true;
     openFirewall = true;
