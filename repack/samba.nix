ab1422ad43a5bd4da0f14a8a72e213dc7034e1e1
oluceps <secinia@outlook.com>
2024-10-02 01:15:42 +0800

    +

--- repack/samba.nix
+++ repack/samba.nix
@@ -1,6 +1,6 @@
 { reIf, ... }:
 reIf {
-  avahi = {
+  services.avahi = {
     publish.enable = true;
     publish.userServices = true;
     # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
