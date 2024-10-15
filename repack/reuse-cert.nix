97caf83fe61f9ee826d60e771a93adfaafab04de
oluceps <secinia@outlook.com>
2024-10-16 04:11:06 +0800

    +

--- repack/reuse-cert.nix
+++ repack/reuse-cert.nix
@@ -26,9 +26,17 @@ reIf (
         acc
         // {
           ${i.name} = {
-            serviceConfig.BindReadOnlyPaths = lib.mkIf i.cond [
-              "-/var/lib/caddy/certificates/acme-v02.api.letsencrypt.org-directory/"
-            ];
+            serviceConfig.BindReadOnlyPaths = lib.mkIf i.cond (
+              map
+                (
+                  s:
+                  "-/var/lib/caddy/certificates/acme-v02.api.letsencrypt.org-directory/wildcard_.nyaw.xyz/wildcard_.nyaw.xyz.${s}"
+                )
+                [
+                  "key"
+                  "crt"
+                ]
+            );
           };
         }
       ) { } nameCondPair)
