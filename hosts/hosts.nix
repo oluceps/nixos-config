808c575cfcee07f578a701c8589d901c0e308106
oluceps <secinia@outlook.com>
2024-10-29 19:16:51 +0800

    +

---
+++ hosts/hosts.nix
@@ -0,0 +1,45 @@
+{
+  kaambl = {
+    "127.0.0.1" = [
+      "kaambl.nyaw.xyz"
+      "dns.nyaw.xyz"
+    ];
+    "10.0.1.1" = [ "nodens.nyaw.xyz" ];
+    "10.0.2.1" = [ "azasos.nyaw.xyz" ];
+    "10.0.3.1" = [ "abhoth.nyaw.xyz" ];
+    "10.0.4.6" = [
+      "eihort.nyaw.xyz"
+      "nas.nyaw.xyz"
+    ];
+    "10.0.1.2" = [
+      "s3.nyaw.xyz"
+    ];
+    "10.0.4.2" = [
+      "hastur.nyaw.xyz"
+    ];
+  };
+  hastur = {
+    "127.0.0.1" = [
+      "s3.nyaw.xyz"
+      "hastur.nyaw.xyz"
+    ];
+    "10.0.1.1" = [ "nodens.nyaw.xyz" ];
+    "10.0.2.3" = [ "kaambl.nyaw.xyz" ];
+    "10.0.2.1" = [ "azasos.nyaw.xyz" ];
+    "10.0.3.1" = [ "abhoth.nyaw.xyz" ];
+    "10.0.4.1" = [ "yidhra.nyaw.xyz" ];
+    "192.168.1.158" = [ "eihort.nyaw.xyz" ];
+  };
+  eihort = {
+    "127.0.0.1" = [
+      "eihort.nyaw.xyz"
+      "nas.nyaw.xyz"
+    ];
+    "10.0.1.1" = [ "nodens.nyaw.xyz" ];
+    "10.0.2.3" = [ "kaambl.nyaw.xyz" ];
+    "10.0.2.1" = [ "azasos.nyaw.xyz" ];
+    "10.0.3.1" = [ "abhoth.nyaw.xyz" ];
+    "10.0.4.1" = [ "yidhra.nyaw.xyz" ];
+    "192.168.1.2" = [ "hastur.nyaw.xyz" ];
+  };
+}
