343ba72e2fde5db0ba1a34a3912fa64045eff210
oluceps <secinia@outlook.com>
2024-11-17 15:19:50 +0800

    +

--- hosts/yidhra/caddy.nix
+++ hosts/yidhra/caddy.nix
@@ -68,40 +68,6 @@
                         {
                           handle = [
                             {
-                              handler = "encode";
-                              encodings = {
-                                zstd = {
-                                  level = "better";
-                                };
-                              };
-                            }
-                            {
-                              handler = "reverse_proxy";
-                              transport = {
-                                protocol = "http";
-                                tls = {
-                                  server_name = "s3.nyaw.xyz";
-                                };
-                              };
-                              upstreams = [ { dial = "10.0.4.2:443"; } ];
-                            }
-                          ];
-                        }
-                      ];
-                    }
-                  ];
-                  match = [ { host = [ "s3.nyaw.xyz" ]; } ];
-                  terminal = true;
-                }
-
-                {
-                  handle = [
-                    {
-                      handler = "subroute";
-                      routes = [
-                        {
-                          handle = [
-                            {
                               handler = "reverse_proxy";
                               # transport = {
                               #   protocol = "http";
