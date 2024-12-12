ce3ee0c47cfa36cf164068bea31e33d104e3f528
oluceps <secinia@outlook.com>
2024-12-13 00:05:34 +0800

    +

--- pkgs/by-name/runbkworm.nix
+++ pkgs/by-name/runbkworm.nix
@@ -7,9 +7,8 @@ pkgs.writeShellApplication {
       -cpu host -smp 6 \
       -m 8G \
       -nographic \
-      -device virtio-net-pci,netdev=net0 \
-      -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:8080 \
-      -drive if=virtio,format=qcow2,file=/var/lib/virt/debian-12-nocloud-amd64-daily-20230911-1500.qcow2 \
+      -nic user,model=virtio-net-pci \
+      -drive if=virtio,format=qcow2,file=/var/lib/virt/debian-12-nocloud-amd64-daily-20230911-1500.qcow2,aio=io_uring \
       "$@"
   '';
 }
