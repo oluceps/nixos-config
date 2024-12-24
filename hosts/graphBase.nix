6e285953e0e521fd163f1eeaf8d47de6570faae7
oluceps <secinia@outlook.com>
2024-12-24 15:11:59 +0800

    +

--- hosts/graphBase.nix
+++ hosts/graphBase.nix
@@ -309,6 +309,7 @@
         addons = with pkgs; [
           fcitx5-chinese-addons
           fcitx5-mozc
+          fcitx5-rime
           fcitx5-gtk
           fcitx5-configtool
           fcitx5-pinyin-zhwiki
