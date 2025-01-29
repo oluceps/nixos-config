07f5ff7811e5d4cc7d96f820b70759d34d6d81cb
oluceps <secinia@outlook.com>
2025-01-29 20:26:19 +0800

    _

--- repack/misskey.nix
+++ repack/misskey.nix
@@ -23,7 +23,7 @@ reIf {
     };
     serviceConfig = {
       LoadCredential = [ "config:${config.vaultix.secrets.misskey.path}" ];
-      ExecStart = "${pkgs.sharkey}/bin/misskey migrateandstart";
+      ExecStart = "${pkgs.misskey}/bin/misskey migrateandstart";
       RuntimeDirectory = "misskey";
       RuntimeDirectoryMode = "700";
       StateDirectory = "misskey";
