37063a7d5f0bc292d413c6326b7dca00c225e040
oluceps <secinia@outlook.com>
2025-02-10 09:53:56 +0800

    + refactor with vaultix ITS

--- age/trust.nix
+++ age/trust.nix
@@ -31,7 +31,22 @@
   "db.pem"
   "dae"
 ])
-// {
-  hyst-osa-cli.file = ../sec/hyst-osa-cli.age;
-  hyst-hk-cli.file = ../sec/hyst-hk-cli.age;
-}
+// (
+  let
+    genHyst = name: addr: {
+      "hyst-${name}-cli" = {
+        file = ../sec/hyst-cli.age;
+        insert = {
+          "f3c4e59bfb78c6a26564724aaadda3ac3250d73ee903b17e3803785335bd082c" = {
+            content = addr;
+            order = 0;
+          };
+        };
+      };
+    };
+  in
+  lib.concatMapAttrs genHyst {
+    osa = "172.234.92.148";
+    hk = "8.210.47.13";
+  }
+)
