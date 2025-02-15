7b1d4a03f9f6cad8b55018fc3695984f61409955
oluceps <secinia@outlook.com>
2025-02-15 13:43:03 +0800

    -

--- age/trust.nix
+++ age/trust.nix
@@ -9,9 +9,6 @@
 }:
 (hard [
   "general.toml"
-  "on-kaambl.toml"
-  "on-eihort.toml"
-  "on-hastur.toml"
   "jc-do"
   "ss-az"
   "naive"
@@ -51,10 +48,6 @@
       "f3c4e59bfb78c6a26564724aaadda3ac3250d73ee903b17e3803785335bd082c" = {
         content = "8.210.47.13";
         order = 0;
-      };
-      "b1ca20eb6f34aa70cc00682636eb3582d592727923789fed0eeb56fa567d5c01" = {
-        content = "";
-        order = 1;
       };
     };
   };
