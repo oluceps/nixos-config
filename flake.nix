1c8966425a4c38077f678fb25f61044649e63fbf
oluceps <secinia@outlook.com>
2025-02-20 01:12:04 +0800

    +

--- flake.nix
+++ flake.nix
@@ -219,7 +219,8 @@
     };
     # path:/home/riro/Src/flake.nix
     # dae.url = "github:daeuniverse/flake.nix";
-    dae.url = "/home/elen/Src/flake.nix";
+    dae.url = "github:oluceps/flake.nix/dev";
+    # dae.url = "/home/elen/Src/flake.nix";
     nixyDomains.url = "github:oluceps/nixyDomains";
     nixyDomains.flake = false;
     nuenv.url = "github:DeterminateSystems/nuenv";
