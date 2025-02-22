36509926800b02b43901ea2ba752f56cac327db8
oluceps <secinia@outlook.com>
2025-02-23 02:15:33 +0800

    +

--- repack/dae.nix
+++ repack/dae.nix
@@ -74,7 +74,11 @@ reIf {
             lib.concatMapStringsSep "," (n: "suffix: ${n}.nyaw.xyz") (builtins.attrNames lib.data.node)
           }) -> direct
 
-          dip(${lib.concatStringsSep "," (map (i: i.addr) (builtins.attrValues lib.data.node))}) -> direct
+          dip(${
+            lib.concatStringsSep "," (
+              map (i: i.addr) (builtins.attrValues (lib.filterAttrs (k: v: v.censor) lib.data.node))
+            )
+          }) -> direct
 
           domain(geosite:cn) -> direct
           dip(geoip:private,geoip:cn) -> direct
