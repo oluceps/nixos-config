eb503c42992c74b9f83de31d55b024eedd4cea07
oluceps <secinia@outlook.com>
2025-02-28 01:23:08 +0800

    +

--- repack/fail2ban.nix
+++ repack/fail2ban.nix
@@ -13,9 +13,8 @@ reIf {
     bantime = "24h";
     bantime-increment = {
       enable = true; # Enable increment of bantime after each violation
-      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
       multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h"; # Do not ban for more than 1 week
+      maxtime = "504h"; # Do not ban for more than 1 week
       overalljails = true; # Calculate the bantime based on all the violations
     };
   };
