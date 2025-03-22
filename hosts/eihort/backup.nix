3ed734f917735a933192c644842c9f9a6fd0546b
oluceps <secinia@outlook.com>
2025-03-22 11:31:59 +0800

    +

--- hosts/eihort/backup.nix
+++ hosts/eihort/backup.nix
@@ -1,4 +1,30 @@
 { config, lib, ... }:
 {
   repack.postgresql-backup.enable = true;
+
+  vaultix.secrets = {
+   "on-yidong.toml" = {
+      file = ../../sec/on-yidong.toml.age;
+    };
+  };
+  services.rustic = {
+    profiles = map (n: config.vaultix.secrets.${n}.path) [
+      "general.toml"
+      "on-yidong.toml"
+    ];
+    backups = {
+      critic = {
+        profiles = map (n: config.vaultix.secrets.${n}.path) [
+          "general.toml"
+          "on-yidong.toml"
+        ];
+        timerConfig = {
+          OnCalendar = "*-*-1/3 03:00:00";
+          RandomizedDelaySec = "4h";
+          FixedRandomDelay = true;
+          Persistent = true;
+        };
+      };
+    };
+  };
 }
