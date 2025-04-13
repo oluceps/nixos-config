8c1189da93f756c66ea3505cbccccaa83739bfa0
oluceps <secinia@outlook.com>
2025-04-14 00:24:37 +0800

    -=

--- repack/misskey.nix
+++ repack/misskey.nix
@@ -8,51 +8,56 @@ reIf {
   services.redis.servers.misskey = {
     enable = true;
     port = 6379;
+    # bind = "0.0.0.0";
+    # extraParams = [ "--protected-mode no" ];
   };
-  users = {
-    groups.misskey = { };
-    users.misskey = {
-      isSystemUser = true;
-      group = "misskey";
-      home = "/var/lib/misskey";
-      linger = true;
-      createHome = true;
-      subUidRanges = [
-        {
-          count = 65536;
-          startUid = 2147483646;
-        }
-      ];
-      subGidRanges = [
-        {
-          count = 65536;
-          startGid = 2147483647;
-        }
-      ];
-    };
-  };
+  # users = {
+  #   groups.misskey = { };
+  #   users.misskey = {
+  #     isSystemUser = true;
+  #     group = "misskey";
+  #     home = "/var/lib/misskey";
+  #     linger = true;
+  #     createHome = true;
+  #     subUidRanges = [
+  #       {
+  #         count = 65536;
+  #         startUid = 2147483646;
+  #       }
+  #     ];
+  #     subGidRanges = [
+  #       {
+  #         count = 65536;
+  #         startGid = 2147483647;
+  #       }
+  #     ];
+  #   };
+  # };
 
   virtualisation.oci-containers = {
     backend = "podman";
     containers.misskey = {
-      volumes = [ "${config.vaultix.secrets.misskey.path}:/misskey/.config/config:ro" ];
+      volumes = [
+        "${config.vaultix.secrets.misskey.path}:/misskey/.config/config:ro"
+        "/etc/ssl/certs:/etc/ssl/certs:ro"
+      ];
       # pull = "always";
       image = "misskey/misskey:2025.4";
-      ports = [ ];
-      networks = [ "pasta:--map-gw" ];
+      # ports = [ "3012:3012" ];
+      networks = [ "host" ];
+      # networks = [ "pasta:--map-gw" ];
 
       environment = {
         MISSKEY_CONFIG_YML = "config";
         NODE_OPTIONS = "--use-openssl-ca";
       };
 
-      podman = {
-        user = "misskey";
-        sdnotify = "healthy";
-      };
       # TODO: CA
     };
   };
+  # systemd.services.podman-misskey.serviceConfig.LoadCredential = [
+  #   "config:${config.vaultix.secrets.misskey.path}"
+  # ];
 
   # systemd.services.misskey = {
   #   after = [
