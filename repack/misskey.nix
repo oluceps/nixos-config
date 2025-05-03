80671770a69a5237e242cb0972c381f48c0166ad
oluceps <secinia@outlook.com>
2025-05-04 03:56:50 +0800

    +

--- repack/misskey.nix
+++ repack/misskey.nix
@@ -9,31 +9,29 @@ reIf {
   services.redis.servers.misskey = {
     enable = true;
     port = 6379;
-    # bind = "0.0.0.0";
-    # extraParams = [ "--protected-mode no" ];
   };
-  # users = {
-  #   groups.misskey = { };
-  #   users.misskey = {
-  #     isSystemUser = true;
-  #     group = "misskey";
-  #     home = "/var/lib/misskey";
-  #     linger = true;
-  #     createHome = true;
-  #     subUidRanges = [
-  #       {
-  #         count = 65536;
-  #         startUid = 2147483646;
-  #       }
-  #     ];
-  #     subGidRanges = [
-  #       {
-  #         count = 65536;
-  #         startGid = 2147483647;
-  #       }
-  #     ];
-  #   };
-  # };
+  users = {
+    groups.misskey = { };
+    users.misskey = {
+      isSystemUser = true;
+      group = "misskey";
+      home = "/var/lib/misskey";
+      linger = true;
+      createHome = true;
+      subUidRanges = [
+        {
+          count = 65536;
+          startUid = 2147483646;
+        }
+      ];
+      subGidRanges = [
+        {
+          count = 65536;
+          startGid = 2147483647;
+        }
+      ];
+    };
+  };
 
   virtualisation.oci-containers = {
     containers.misskey = {
@@ -50,57 +48,21 @@ reIf {
       ];
       # pull = "always";
       image = "misskey/misskey:2025.4.1";
-      # ports = [ "3012:3012" ];
-      networks = [ "host" ];
-      # networks = [ "pasta:--map-gw" ];
+      networks = [
+        "pasta:-T,5432,-T,6379,-T,7700"
+      ];
+      podman = {
+        user = "misskey";
+        sdnotify = "healthy";
+      };
 
       environment = {
         MISSKEY_CONFIG_YML = "config";
-        # NODE_OPTIONS = "--use-openssl-ca";
         NODE_EXTRA_CA_CERTS = "/misskey/ca.crt";
       };
-
-      # TODO: CA
     };
   };
   # systemd.services.podman-misskey.serviceConfig.LoadCredential = [
   #   "config:${config.vaultix.secrets.misskey.path}"
   # ];
-
-  # systemd.services.misskey = {
-  #   after = [
-  #     "network-online.target"
-  #     "postgresql.service"
-  #   ];
-  #   wants = [ "network-online.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   environment = {
-  #     MISSKEY_CONFIG_YML = "/run/credentials/misskey.service/config";
-  #     NODE_OPTIONS = "--use-openssl-ca";
-  #   };
-  #   serviceConfig = {
-  #     LoadCredential = [ "config:${config.vaultix.secrets.misskey.path}" ];
-  #     ExecStart = "${pkgs.misskey}/bin/misskey migrateandstart";
-  #     RuntimeDirectory = "misskey";
-  #     RuntimeDirectoryMode = "770";
-  #     StateDirectory = "misskey";
-  #     StateDirectoryMode = "700";
-  #     TimeoutSec = 60;
-  #     DynamicUser = false;
-  #     User = "misskey";
-  #     Group = "misskey";
-  #     LockPersonality = true;
-  #     PrivateDevices = true;
-  #     PrivateUsers = true;
-  #     ProtectClock = true;
-  #     ProtectControlGroups = true;
-  #     ProtectHome = true;
-  #     ProtectHostname = true;
-  #     ProtectKernelLogs = true;
-  #     ProtectProc = "invisible";
-  #     ProtectKernelModules = true;
-  #     ProtectKernelTunables = true;
-  #     RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX AF_NETLINK";
-  #   };
-  # };
 }
