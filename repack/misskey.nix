3df3906a65263dedc46b7eda51c800ea71a007bc
oluceps <secinia@outlook.com>
2025-05-05 18:06:40 +0800

    +

--- repack/misskey.nix
+++ repack/misskey.nix
@@ -55,9 +55,10 @@ reIf {
         "3012:3012"
       ];
       networks = [
-        "pasta:-T,5432,-T,6379,-T,7700,-T,443"
+        # "pasta:-T,5432,-T,6379,-T,7700,-T,443"
+        "host"
       ];
-      extraOptions = [ "--add-host=s3.nyaw.xyz:127.0.0.1" ];
+      # extraOptions = [ "--add-host=s3.nyaw.xyz:127.0.0.1" ];
       podman = {
         user = "misskey";
         sdnotify = "healthy";
