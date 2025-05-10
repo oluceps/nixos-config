baaf729128ba2508f3ecad459a54a5af81875a4d
oluceps <secinia@outlook.com>
2025-05-11 05:34:11 +0800

    +

--- misc.nix
+++ misc.nix
@@ -87,6 +87,9 @@
         set fish_cursor_default block blink
         set fish_cursor_insert line blink
         set fish_cursor_replace_one underscore blink
+        set fish_pager_color_selected_background --reverse
+        set fish_cursor_replace_one underscore blink
+
 
         # fish_config theme choose rose-pine-dawn
 
