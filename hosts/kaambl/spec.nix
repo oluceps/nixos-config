fe33ec12af529e6ba13ecf6afbc1b4a5461e448e
oluceps <secinia@outlook.com>
2025-05-22 04:41:13 +0800

    +

--- hosts/kaambl/spec.nix
+++ hosts/kaambl/spec.nix
@@ -58,6 +58,8 @@
   users.users.${user}.extraGroups = [ "video" ];
   users.groups.video = { };
 
+  environment.systemPackages = with pkgs; [ texlive.combined.scheme-medium ];
+
   services = {
     userborn.enable = true;
     logind = {
