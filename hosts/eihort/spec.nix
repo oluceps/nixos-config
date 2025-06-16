68ceac2621716436c01d7a8b62c8407bcb64b39f
oluceps <secinia@outlook.com>
2025-06-17 02:09:40 +0800

    -

--- hosts/eihort/spec.nix
+++ hosts/eihort/spec.nix
@@ -86,7 +86,7 @@
     fail2ban.enable = true;
     # phantomsocks.enable = true;
     dae.enable = true;
-    # dnsproxy.enable = true;
+    dnsproxy.enable = true;
     scrutiny.enable = true;
     postgresql.enable = true;
 
