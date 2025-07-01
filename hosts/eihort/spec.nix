ae2690ea79b3f117863da70ab541c6bbc1d5231f
oluceps <secinia@outlook.com>
2025-07-02 04:36:05 +0800

    + iscsi
    +

--- hosts/eihort/spec.nix
+++ hosts/eihort/spec.nix
@@ -54,7 +54,7 @@
   system = {
 
     etc.overlay.enable = true;
-    etc.overlay.mutable = false;
+    etc.overlay.mutable = true;
 
     stateVersion = "25.05";
   };
@@ -115,6 +115,16 @@
       enable = true;
       socketActivated = true;
     };
+    target = {
+      enable = true;
+      # ugly
+      config = builtins.fromJSON (builtins.readFile ./target_cfg.json);
+    };
+    # openiscsi = {
+    #   enable = true;
+    #   discoverPortal = "ip:3260";
+    #   name = "iqn.2005-10.org.nixos.ctl:ntfs-games";
+    # };
     rqbit = {
       enable = true;
       location = "/three/storage/Downloads";
