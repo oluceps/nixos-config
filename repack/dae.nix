ecbcaff06501c4188c742f959e23fbf1c06a7e64
oluceps <secinia@outlook.com>
2025-07-08 03:23:23 +0800

    -

--- repack/dae.nix
+++ repack/dae.nix
@@ -23,7 +23,7 @@ reIf {
           tproxy_port: 12345
           log_level: debug
           #tcp_check_url: 'https://www.apple.com/library/test/success.html,2606:4700:4700::1111'
-          udp_check_dns: '8.8.8.8:53,114.114.114.114:53,2001:4860:4860::8888,1.1.1.1:53'
+          udp_check_dns: '8.8.8.8:53,2001:4860:4860::8888,1.1.1.1:53'
           check_interval: 30s
           check_tolerance: 50ms
           wan_interface: auto
