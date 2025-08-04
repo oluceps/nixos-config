7a1067cde9736c81deef053016f290adf386cf44
oluceps <secinia@outlook.com>
2025-08-05 04:20:07 +0800

    +

--- hosts/caddy/matrix.nix
+++ hosts/caddy/matrix.nix
@@ -18,74 +18,54 @@
           ];
           terminal = true;
         }
-      ];
-    }
 
-    {
-      handler = "headers";
-      response.set = {
-        X-Frame-Options = [ "SAMEORIGIN" ];
-        X-Content-Type-Options = [ "nosniff" ];
-        X-XSS-Protection = [ "1; mode=block" ];
-        Content-Security-Policy = [ "frame-ancestors 'self'" ];
-      };
-    }
-    {
-      handler = "subroute";
-      routes = [
         {
           handle = [
             {
               handler = "rewrite";
-              uri = "/olm.wasm";
-            }
-          ];
-          match = [ { path = [ "/*/olm.wasm" ]; } ];
-        }
-        {
-          handle = [
-            {
-              handler = "rewrite";
-              uri = "/index.html";
+              uri = "{http.matchers.file.relative}";
             }
           ];
           match = [
             {
-              not = [
-                { path = [ "/index.html" ]; }
-                { path = [ "/public/*" ]; }
-                { path = [ "/assets/*" ]; }
-                { path = [ "/config.json" ]; }
-                { path = [ "/manifest.json" ]; }
-                { path = [ "/pdf.worker.min.js" ]; }
-                { path = [ "/olm.wasm" ]; }
-              ];
-              path = [ "/*" ];
+              file = {
+                try_files = [
+                  "{http.request.uri.path}"
+                  "/"
+                  "index.html"
+                ];
+              };
             }
           ];
         }
+        {
+          handle = [
+            (
+              let
+                conf = {
+                  defaultHomeserver = 0;
+                  homeserverList = [
+                    "nyaw.xyz"
+                    "converser.eu"
+                    "envs.net"
+                    "matrix.org"
+                    "monero.social"
+                    "mozilla.org"
+                    "xmr.se"
+                  ];
+                };
+              in
+              {
+                handler = "file_server";
+                root = pkgs.cinny.override { inherit conf; };
+              }
+            )
+          ];
+        }
+
       ];
     }
-    (
-      let
-        conf = {
-          defaultHomeserver = 0;
-          homeserverList = [
-            "nyaw.xyz"
-            "converser.eu"
-            "envs.net"
-            "matrix.org"
-            "monero.social"
-            "mozilla.org"
-            "xmr.se"
-          ];
-        };
-      in
-      {
-        handler = "file_server";
-        root = "${pkgs.cinny.override { inherit conf; }}";
-      }
-    )
+
   ];
   match = [ { host = [ "matrix.nyaw.xyz" ]; } ];
   terminal = true;
