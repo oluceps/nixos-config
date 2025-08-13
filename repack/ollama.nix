7c47ad86cc2d4ea5f83edda5fdbfe1b1592c8d85
oluceps <secinia@outlook.com>
2025-08-13 20:28:24 +0800

    + ollama

--- repack/ollama.nix
+++ repack/ollama.nix
@@ -1,27 +1,35 @@
 {
-  pkgs,
-  lib,
   reIf,
   ...
 }:
 reIf {
-  services.ollama = {
-    enable = true;
-    package = pkgs.ollama-ipex;
-  };
-  systemd.services.ollama.serviceConfig = lib.mkForce {
-    DynamicUser = true;
+  virtualisation.oci-containers.containers.ipex-llm-container = {
 
-    ExecStart = "${pkgs.ollama-ipex}/bin/ollama-ipex serve";
+    image = "intelanalytics/ipex-llm-inference-cpp-xpu:latest";
 
-    PrivateTmp = true;
-
-    Environment = [
-      "HOME=/var/lib/ollama"
-      "OLLAMA_HOST=[::]:11434"
-      "OLLAMA_MODELS=/var/lib/ollama/models"
-      "OLLAMA_ORIGINS=\"http://192.168.*\""
+    volumes = [ "/var/lib/ollama/models:/models" ];
+    # # pull = "always";
+    # # privileged = true;
+    environment = {
+      DEVICE = "Arc";
+      no_proxy = "localhost,127.0.0.1";
+      OLLAMA_ORIGINS = "http://192.168.*";
+      SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS = "1";
+      ONEAPI_DEVICE_SELECTOR = "level_zero:0";
+      OLLAMA_HOST = "[::]:11434";
+    };
+    devices = [
+      "/dev/dri:/dev/dri:rwm"
     ];
-    ReadWritePaths = [ "/var/lib/ollama" ];
+    cmd = [
+      "/bin/sh"
+      "-c"
+      "/llm/scripts/start-ollama.sh && echo 'Startup script finished, container is now idling.' && sleep infinity"
+    ];
+    extraOptions = [
+      "--net=host"
+      "--memory=32G"
+      "--shm-size=16g"
+    ];
   };
 }
