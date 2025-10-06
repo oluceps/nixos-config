01e08b50fd6b815e8a8d35556a02c43739c98f8c
oluceps <secinia@outlook.com>
2025-10-07 01:31:29 +0800

    +=

--- repack/alloy.nix
+++ repack/alloy.nix
@@ -11,26 +11,28 @@ reIf {
       livedebugging {
         enabled = false
       }
-      // discovery.relabel "journal" {
-      // 	targets = []
-      // 	rule {
-      // 		source_labels = ["__journal__systemd_unit"]
-      // 		target_label  = "unit"
-      // 	}
-      // }
-      // loki.source.journal "journal" {
-      //     max_age       = "12h0m0s"
-      //     relabel_rules = discovery.relabel.journal.rules
-      //     forward_to    = [loki.write.default.receiver]
-      //     labels        = {
-      //         host = "hastur",
-      //         job  = "systemd-journal",
-      //     }
-      // }
+      loki.source.journal "journal" {
+          max_age       = "12h0m0s"
+          labels        = {
+              host = "hastur",
+              job  = "refused",
+          }
+          forward_to = [loki.process.refused.receiver]
+      }
+      loki.process "refused" {
+        stage.match {
+          selector = ".*refused connection.*"
+        }
+
+        forward_to = [loki.write.default.receiver]
+      }
+
       local.file_match "zeek_logs" {
         path_targets = [
-          { "__path__" = "/var/log/zeek/dns.log" },
-          { "__path__" = "/var/log/zeek/ssl.log" },
+          { "__path__" = "/var/log/zeek/vm1/dns.log" },
+          { "__path__" = "/var/log/zeek/vm1/ssl.log" },
+          { "__path__" = "/var/log/zeek/eno1/dns.log" },
+          { "__path__" = "/var/log/zeek/eno1/ssl.log" },
         ]
         sync_period = "5s"
       }
@@ -43,7 +45,8 @@ reIf {
 
       local.file_match "zeek_conns" {
         path_targets = [
-          { "__path__" = "/var/log/zeek/conn.log" },
+          { "__path__" = "/var/log/zeek/vm1/conn.log" },
+          { "__path__" = "/var/log/zeek/eno1/conn.log" },
         ]
         sync_period = "5s"
       }
