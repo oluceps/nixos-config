9cf8eb9bc3e3c9d16399cb7663c5afeed5b76faa
oluceps <secinia@outlook.com>
2025-10-10 05:55:01 +0800

    =

--- overlays.nix
+++ overlays.nix
@@ -33,6 +33,7 @@
       #   ;
       inherit (inputs'.browser-previews.packages) google-chrome-beta;
       inherit (inputs'.nixpkgs-stable.legacyPackages) meilisearch minio;
+      # inherit (inputs'.nixpkgs-master.legacyPackages) linuxPackages_latest;
       tuwunel = inputs'.conduit.packages.default;
 
       sing-box = prev.sing-box.overrideAttrs (old: rec {
--- repack/alloy.nix
+++ repack/alloy.nix
@@ -11,22 +11,22 @@ reIf {
       livedebugging {
         enabled = false
       }
+      discovery.relabel "journal" {
+      	targets = []
+      	rule {
+      		source_labels = ["__journal__systemd_unit"]
+      		target_label  = "unit"
+      	}
+      }
       loki.source.journal "journal" {
           max_age       = "12h0m0s"
+          relabel_rules = discovery.relabel.journal.rules
+          forward_to    = [loki.write.default.receiver]
           labels        = {
               host = "hastur",
-              job  = "refused",
+              job  = "systemd-journal",
           }
-          forward_to = [loki.process.refused.receiver]
       }
-      loki.process "refused" {
-        stage.match {
-          selector = ".*refused connection.*"
-        }
-
-        forward_to = [loki.write.default.receiver]
-      }
-
       local.file_match "zeek_logs" {
         path_targets = [
           { "__path__" = "/var/log/zeek/vm1/dns.log" },
