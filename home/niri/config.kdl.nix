7a0ad96b63f28c3faa22915a3d727fa2a923c607
oluceps <secinia@outlook.com>
2025-11-12 04:49:50 +0800

    +

--- home/niri/config.kdl.nix
+++ home/niri/config.kdl.nix
@@ -30,7 +30,11 @@ let
   execDesktop =
     app:
     execApp [
-      "niri msg action spawn --"
+      "niri"
+      "msg"
+      "action"
+      "spawn"
+      "--"
       app
     ];
 in
@@ -251,8 +255,9 @@ in
   // which may be more convenient to use.
   spawn-at-startup ${execApp [ (lib.getExe pkgs.foot) ]}
   spawn-at-startup ${execDesktop "chromium-browser"}
-  spawn-at-startup ${execDesktop "org.telegram.desktop.desktop"}
+  spawn-at-startup ${execDesktop "Telegram"}
   spawn-at-startup ${execDesktop "thunderbird"}
+  spawn-sh-at-startup "vicinae server"
   cursor {
       // Change the theme and size of the cursor as well as set the
       // `XCURSOR_THEME` and `XCURSOR_SIZE` env variables.
@@ -294,11 +299,12 @@ in
 
       // Suggested binds for running programs: terminal, app launcher, screen locker.
       Mod+Return { spawn ${execApp [ (lib.getExe pkgs.foot) ]}; }
-      Mod+D { spawn "noctalia-shell" "ipc" "call" "launcher" "toggle"; }
+      Mod+D repeat=false { spawn "vicinae" "toggle"; }
       Ctrl+Shift+L { spawn "loginctl" "lock-session"; }
       Mod+W { toggle-column-tabbed-display; }
       Mod+T { toggle-overview; }
       Mod+P { spawn "noctalia-shell" "ipc" "call" "sessionMenu" "toggle"; }
+      
 
 
       Mod+WheelScrollDown cooldown-ms=150 { focus-column-right; }
@@ -316,7 +322,7 @@ in
 
       XF86MonBrightnessUp { spawn "light" "-A" "3"; }
       XF86MonBrightnessdown { spawn "light" "-U" "3"; }
-      Mod+Ctrl+P { spawn "noctalia-shell" "ipc" "call" "launcher" "clipboard"; }
+      Mod+Ctrl+P repeat=false { spawn "vicinae" "vicinae://extensions/vicinae/clipboard/history"; }
 
       Mod+Q { close-window; }
 
