553a0ee519288b19cedeb0cc9dfa85fb54eaf9c0
oluceps <secinia@outlook.com>
2026-02-14 09:15:04 +0800

    +

--- hosts/eihort/spec.nix
+++ hosts/eihort/spec.nix
@@ -50,27 +50,27 @@
       trim = false;
     };
 
-    hyst-no = {
-      content =
-        config.vaultix.placeholder.hyst-no-cli
-        + (
-          let
-            port = toString (lib.conn { }).${config.networking.hostName}.nodens;
-          in
-          ''
-            socks5:
-              listen: 127.0.0.1:1093
-            udpForwarding:
-            - listen: 127.0.0.1:${port}
-              remote: 127.0.0.1:${port}
-              timeout: 120s
-          ''
-        );
-      owner = "root";
-      group = "users";
-      name = "no.yaml";
-      trim = false;
-    };
+    # hyst-no = {
+    #   content =
+    #     config.vaultix.placeholder.hyst-no-cli
+    #     + (
+    #       let
+    #         port = toString (lib.conn { }).${config.networking.hostName}.nodens;
+    #       in
+    #       ''
+    #         socks5:
+    #           listen: 127.0.0.1:1093
+    #         udpForwarding:
+    #         - listen: 127.0.0.1:${port}
+    #           remote: 127.0.0.1:${port}
+    #           timeout: 120s
+    #       ''
+    #     );
+    #   owner = "root";
+    #   group = "users";
+    #   name = "no.yaml";
+    #   trim = false;
+    # };
   };
   security.auditd.enable = true;
   system = {
@@ -227,10 +227,10 @@
         enable = true;
         configFile = config.vaultix.templates.hyst-yi.path;
       };
-      nodens = {
-        enable = true;
-        configFile = config.vaultix.templates.hyst-no.path;
-      };
+      # nodens = {
+      #   enable = true;
+      #   configFile = config.vaultix.templates.hyst-no.path;
+      # };
     };
 
     # minio = {
