1efdc829e7c255f1c7af23b4faaa5ace464fb33a
oluceps <secinia@outlook.com>
2026-03-13 01:37:19 +0800

    +

--- repack/caddy.nix
+++ repack/caddy.nix
@@ -21,18 +21,17 @@ in
       expose = lib.mkEnableOption "API env";
       package = lib.mkOption {
         type = lib.types.package;
-        default = pkgs.caddy
-        # .withPlugins {
-        #   plugins = [
-        #     "github.com/caddy-dns/cloudflare@v0.0.0-20250724223520-f589a18c0f5d"
-        #     "github.com/mholt/caddy-ratelimit@v0.1.0"
-        #     "github.com/ss098/certmagic-s3@v0.0.0-20250607141218-0c4ff782fbd0"
-        #     "github.com/greenpau/caddy-security@v1.1.31"
-        #     "github.com/mholt/caddy-l4@v0.0.0-20260112235400-e24201789f06"
-        #   ];
-        #   hash = "";
-        # };
-        ;
+        default = pkgs.caddy.withPlugins {
+          plugins = [
+            "github.com/caddy-dns/cloudflare@v0.0.0-20250724223520-f589a18c0f5d"
+            "github.com/mholt/caddy-ratelimit@v0.1.0"
+            "github.com/ss098/certmagic-s3@v0.0.0-20250607141218-0c4ff782fbd0"
+            "github.com/greenpau/caddy-security@v1.1.31"
+            "github.com/mholt/caddy-l4@v0.0.0-20260112235400-e24201789f06"
+          ];
+          hash = "";
+        };
+        # ;
       };
       settings = lib.mkOption {
         type = lib.types.submodule { freeformType = format.type; };
