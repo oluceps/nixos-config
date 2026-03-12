61b61fbae27f1e3c959aaad18455b521bc05fa69
oluceps <secinia@outlook.com>
2026-03-13 01:32:14 +0800

    ++

--- hosts/hastur/spec.nix
+++ hosts/hastur/spec.nix
@@ -218,7 +218,7 @@
       RuntimeWatchdogSec = "30s";
     };
 
-    sleep.settings.Sleep.AllowHibernation = "no";
+    # sleep.settings.Sleep.AllowHibernation = "no";
   };
   repack = {
     plugIn.enable = true;
