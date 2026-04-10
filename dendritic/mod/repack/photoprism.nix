b615d7614fc1d9d0e041ac1824a984eddd3ad68c
oluceps <secinia@outlook.com>
2026-04-11 04:05:31 +0800

    ++

--- dendritic/mod/repack/photoprism.nix
+++ dendritic/mod/repack/photoprism.nix
@@ -1,19 +1,24 @@
-{ ... }:
+{ lib, ... }:
 {
-  flake.modules.nixos.photoprism = {
-    services.photoprism = {
-      enable = true;
-      originalsPath = "/var/lib/private/photoprism/originals";
-      address = "[::]";
-      settings = {
-        PHOTOPRISM_ADMIN_USER = "prism";
-        PHOTOPRISM_DEFAULT_LOCALE = "en";
-        PHOTOPRISM_DATABASE_NAME = "photoprism";
-        PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
-        PHOTOPRISM_DATABASE_USER = "photoprism";
-        PHOTOPRISM_DATABASE_DRIVER = "mysql";
+  flake.modules.nixos.photoprism =
+    { config, lib, ... }:
+    {
+      options.repack.photoprism.enable = lib.mkEnableOption "photoprism";
+      config = lib.mkIf config.repack.photoprism.enable {
+        services.photoprism = {
+          enable = true;
+          originalsPath = "/var/lib/private/photoprism/originals";
+          address = "[::]";
+          settings = {
+            PHOTOPRISM_ADMIN_USER = "prism";
+            PHOTOPRISM_DEFAULT_LOCALE = "en";
+            PHOTOPRISM_DATABASE_NAME = "photoprism";
+            PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+            PHOTOPRISM_DATABASE_USER = "photoprism";
+            PHOTOPRISM_DATABASE_DRIVER = "mysql";
+          };
+          port = 20800;
+        };
       };
-      port = 20800;
     };
-  };
 }
--- dendritic/mod/repack/subs.nix
+++ dendritic/mod/repack/subs.nix
@@ -1,36 +1,36 @@
+{ lib, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  cfg = config.subs;
-in
-{
-  options.subs = {
-    enable = lib.mkEnableOption "subs";
-    scriptPath = lib.mkOption {
-      type = lib.types.str;
-      default = "/var/lib/private/subs/subs.ts";
-    };
-    user = lib.mkOption {
-      type = lib.types.str;
-      default = "riro";
-    };
-  };
+  flake.modules.nixos.subs =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      cfg = config.repack.subs;
+    in
+    {
+      options.repack.subs = {
+        enable = lib.mkEnableOption "subs";
+        scriptPath = lib.mkOption {
+          type = lib.types.str;
+          default = "/var/lib/private/subs/subs.ts";
+        };
+      };
 
-  flake.modules.nixos.subs = lib.mkIf cfg.enable {
-    systemd.services.subs = {
-      unitConfig.StartLimitIntervalSec = 0;
-      serviceConfig.Type = "simple";
-      serviceConfig.User = cfg.user;
-      serviceConfig.ExecStart = "${lib.getExe pkgs.deno} run --allow-env --allow-net --no-check ${cfg.scriptPath}";
-      serviceConfig.Restart = "always";
-      serviceConfig.RestartSec = 1;
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
+      config = lib.mkIf cfg.enable {
+        systemd.services.subs = {
+          unitConfig.StartLimitIntervalSec = 0;
+          serviceConfig.Type = "simple";
+          serviceConfig.User = config.identity.user;
+          serviceConfig.ExecStart = "${lib.getExe pkgs.deno} run --allow-env --allow-net --no-check ${cfg.scriptPath}";
+          serviceConfig.Restart = "always";
+          serviceConfig.RestartSec = 1;
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+        };
+      };
     };
-  };
 }
