5771406750f4569255b88391a1e1aaf8d62cb367
oluceps <secinia@outlook.com>
2026-04-13 03:01:18 +0800

    + migrate repack: misskey

---
+++ dendritic/mod/repack/misskey.nix
@@ -0,0 +1,44 @@
+{
+  flake.modules.nixos.misskey =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      vaultix.secrets.misskey = {
+        owner = "root";
+        mode = "400";
+      };
+      services.redis.servers.misskey = {
+        enable = true;
+        port = 6379;
+      };
+      virtualisation.oci-containers = {
+        containers.misskey = {
+          volumes =
+            let
+              cabundle = pkgs.cacert.override {
+                extraCertificateFiles = with lib.data.ca_cert; [
+                  lib.data.ca_cert.root_file
+                ];
+              };
+            in
+            [
+              "${config.vaultix.secrets.misskey.path}:/misskey/.config/config:ro"
+              "${cabundle}/etc/ssl/certs/ca-bundle.crt:/misskey/ca.crt:ro"
+              "${cabundle}/etc/ssl/certs/ca-bundle.crt:/etc/ssl/certs/ca-certificates.crt"
+            ];
+          image = "misskey/misskey:2026.3.1";
+          networks = [
+            "host"
+          ];
+          environment = {
+            MISSKEY_CONFIG_YML = "config";
+            NODE_EXTRA_CA_CERTS = "/misskey/ca.crt";
+          };
+        };
+      };
+    };
+}
