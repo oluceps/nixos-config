438240bdbbe23d8c5cf97990e85a4beaceb7e0eb
oluceps <secinia@outlook.com>
2026-04-13 02:46:04 +0800

    + migrate repack: vaultwarden

---
+++ dendritic/mod/repack/vaultwarden.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.nixos.vaultwarden =
+    { config, ... }:
+    {
+      vaultix.secrets.vault = {
+        owner = "vaultwarden";
+        mode = "400";
+      };
+      services.vaultwarden = {
+        enable = true;
+        dbBackend = "postgresql";
+        config = {
+          signupsAllowed = false;
+          sendsAllowed = true;
+          emergencyAccessAllowed = false;
+          orgCreationUsers = "none";
+          domain = "https://vault.nyaw.xyz";
+          rocketAddress = "fdcc::3";
+          rocketPort = 8003;
+        };
+        # backupDir = "/var/lib/bitwarden_rs/backup";
+        environmentFile = config.vaultix.secrets.vault.path;
+      };
+    };
+}
