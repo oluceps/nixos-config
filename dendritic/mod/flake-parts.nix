7f49b5f12207a85b501ad0b3ada469e520901d3a
oluceps <secinia@outlook.com>
2026-04-15 23:13:35 +0800

    + finish

--- dendritic/mod/flake-parts.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, ... }:
-{
-  imports = [
-    # https://flake.parts/options/flake-parts-modules.html
-    inputs.flake-parts.flakeModules.modules
-    inputs.vaultix.flakeModules.default
-  ];
-}
